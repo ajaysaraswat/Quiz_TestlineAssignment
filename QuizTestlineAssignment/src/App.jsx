0c6faabcfa2a20ed76295de0ede79009ed02e272
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-02-01 07:14:45 +0530

    making header component

--- QuizTestlineAssignment/src/App.jsx
+++ QuizTestlineAssignment/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css"; // Ensure Tailwind is imported
+import backgroundImage from "./assets/Background.png";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div
-      className="bg-cover bg-center h-screen"
-      style={{ backgroundImage: "url('/assets/Background.png')" }}
-    ></div>
+      className="bg-cover h-screen"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <Header />
+    </div>
   );
 };
 
---
+++ QuizTestlineAssignment/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center justify-center font-bold">
+      <h1 className="text-4xl text-gray-800">QUIZ FLOW APP</h1>
+    </div>
+  );
+};
+
+export default Header;
