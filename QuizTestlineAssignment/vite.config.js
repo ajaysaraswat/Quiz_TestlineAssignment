60da21a973711a6b95b841a48f7149fffa49c3cd
Ajay Kumar Saraswat <146002431+ajaysaraswat@users.noreply.github.com>
2025-02-02 13:17:49 +0530

    Update vite.config.js

--- QuizTestlineAssignment/vite.config.js
+++ QuizTestlineAssignment/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss/vite";  // Correct import for Tailwind CSS
+
 export default defineConfig({
   plugins: [
-    plugins: [react()],
+    react(),
     tailwindcss()
   ],
 });
